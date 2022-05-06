96145c17ed66ffbf03cb68de45e82a0488852f9d
Anthony Carlisi <anthonycarlisi95@gmail.com>
2022-05-05 20:17:06 -0400

    commit

--- src/components/SmsChatWindow.js
+++ src/components/SmsChatWindow.js
@@ -1,5 +1,13 @@
-import React from 'react';
-import {View, Text, StyleSheet, ScrollView, Image} from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  Image,
+  TextInput,
+  FlatList,
+} from 'react-native';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import {
   faChevronLeft,
@@ -9,37 +17,39 @@ import {
   faPhone,
   faCircleArrowUp,
   faChevronDown,
+  faCircleExclamation,
 } from '@fortawesome/free-solid-svg-icons';
 
 function SmsChatWindow() {
   let margin1 = -20;
 
+  const [text, onChangeText] = React.useState('Useless Text');
+  const [number, onChangeNumber] = React.useState(null);
+
   const imageArray = [
     'https://picsum.photos/10',
-    'https://picsum.photos/20',
-    'https://picsum.photos/30',
-    'https://picsum.photos/40',
+    // 'https://picsum.photos/20',
+    // 'https://picsum.photos/30',
+    // 'https://picsum.photos/40',
   ];
 
   return (
-    <View>
+    <View style={styles.window}>
       <View style={styles.header}>
-        <View style={[styles.center, {position: 'absolute', left: 0, top: 30}]}>
-          <FontAwesomeIcon
-            style={{position: 'absolute', left: 0}} // on left, still center vertically.
-            icon={faChevronLeft}
-          />
-          <Text style={[styles.notificationCircle]}>21</Text>
+        <View style={styles.headerLeftNav}>
+          <FontAwesomeIcon style={styles.faChevronLeft} icon={faChevronLeft} />
+          <Text style={styles.notificationCircle}>21</Text>
         </View>
         <View style={styles.center}>
-          <View style={styles.rowCenter}>
+          <View style={styles.avatarContainer}>
             {imageArray.map((image, index) => {
               return (
                 <Image
+                  key={index}
                   style={
                     index != 0
-                      ? [styles.tinyLogo, {marginLeft: -20}]
-                      : [styles.tinyLogo]
+                      ? [styles.headerAvatar, {marginRight: -20}]
+                      : [styles.headerAvatar]
                   }
                   source={{
                     uri: image,
@@ -48,16 +58,16 @@ function SmsChatWindow() {
               );
             })}
           </View>
-          <View style={[styles.rowCenter]}>
+          <View style={styles.rowCenter}>
             <Text style={styles.titleText}>Center</Text>
             <FontAwesomeIcon icon={faChevronRight} />
           </View>
-          <View style={[styles.rowCenter, styles.displayNone]}>
-            <View style={[styles.center, {marginRight: 20}]}>
+          <View style={[styles.headerNavHiddenBar, styles.displayNone]}>
+            <View style={styles.center}>
               <FontAwesomeIcon icon={faPhone} />
               <Text>call</Text>
             </View>
-            <View style={[styles.center, {marginRight: 20}]}>
+            <View style={styles.center}>
               <FontAwesomeIcon icon={faUser} />
               <Text>profile</Text>
             </View>
@@ -68,14 +78,56 @@ function SmsChatWindow() {
           </View>
         </View>
       </View>
-      <ScrollView>
-        <Text>Hello World</Text>
+      <ScrollView style={styles.textWindow}>
+        <View style={styles.lastText}>
+          <Text style={[styles.textTitle, styles.group]}>Anthony Carlisi</Text>
+          <View style={styles.recipientTextSection}>
+            <Image
+              style={styles.textAvatar}
+              source={{
+                uri: 'https://picsum.photos/90',
+              }}
+            />
+            <View style={styles.recipientTextContainer}>
+              <View style={styles.leftArrow}></View>
+              <View style={styles.leftArrowOverlap}></View>
+              <Text style={[styles.text, styles.recipientText]}>lorem50</Text>
+            </View>
+          </View>
+          <Text style={[styles.textTitle, styles.group]}>
+            Received 10:37 AM
+          </Text>
+        </View>
+
+        <View style={styles.lastText}>
+          <View style={styles.senderTextSection}>
+            <FontAwesomeIcon
+              style={styles.textError}
+              icon={faCircleExclamation}
+            />
+            <View style={styles.senderTextContainer}>
+              <View style={styles.rightArrow}></View>
+              <View style={styles.rightArrowOverlap}></View>
+              <Text style={[styles.text, styles.senderText]}>Lorem50</Text>
+            </View>
+          </View>
+          <Text style={[styles.textTitle, styles.end]}>Received 10:37 AM</Text>
+        </View>
       </ScrollView>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  window: {
+    height: '100%',
+  },
+  textWindow: {paddingHorizontal: 15},
   notificationCircle: {
     borderRadius: 50,
     paddingHorizontal: 8,
@@ -92,10 +144,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: '#f9f9f9',
     overflow: 'hidden',
-    borderBottomWidth: 1,
+    borderBottomWidth: 0.001,
     borderBottomColor: '#7a7a78',
   },
-  tinyLogo: {
+  headerAvatar: {
     width: 50,
     height: 50,
     borderRadius: 50,
@@ -103,15 +155,138 @@ const styles = StyleSheet.create({
     borderWidth: 3,
     borderColor: '#f9f9f9',
   },
+  textAvatar: {
+    width: 25,
+    height: 25,
+    borderRadius: 50,
+    overflow: 'hidden',
+    alignSelf: 'flex-end',
+    marginRight: 5,
+  },
   rowCenter: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  recipientTextContainer: {
+    flexDirection: 'row',
+  },
+  headerNavHiddenBar: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    width: 160,
+  },
   center: {
     alignItems: 'center',
     justifyContent: 'center',
   },
+  headerLeftNav: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    left: 0,
+    top: 30,
+  },
   displayNone: {display: 'none'},
+  input: {
+    bottom: 0,
+    backgroundColor: 'pink',
+    alignItems: 'flex-end',
+  },
+  text: {
+    fontSize: 16,
+    borderRadius: 15,
+    paddingHorizontal: 8,
+    paddingVertical: 3,
+    flexWrap: 'wrap',
+  },
+  recipientText: {
+    backgroundColor: '#e8e8ea',
+  },
+  senderText: {
+    backgroundColor: '#248bf5',
+    color: '#f9f9f9',
+  },
+  avatarContainer: {
+    flexDirection: 'row-reverse',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  leftArrow: {
+    alignSelf: 'flex-end',
+    position: 'absolute',
+    left: -4,
+    height: 15,
+    width: 13,
+    backgroundColor: '#e8e8ea',
+    // backgroundColor: 'green',
+    borderBottomRightRadius: 10,
+  },
+  leftArrowOverlap: {
+    alignSelf: 'flex-end',
+    position: 'absolute',
+    left: -4,
+    height: 15,
+    width: 4,
+    backgroundColor: 'white',
+    // backgroundColor: 'blue',
+    borderBottomRightRadius: 20,
+  },
+  rightArrow: {
+    alignSelf: 'flex-end',
+    position: 'absolute',
+    right: -4,
+    height: 15,
+    width: 13,
+    backgroundColor: '#248bf5',
+    // backgroundColor: 'green',
+    borderBottomLeftRadius: 10,
+  },
+  rightArrowOverlap: {
+    zIndex: 0,
+    alignSelf: 'flex-end',
+    position: 'absolute',
+    right: -5,
+    height: 15,
+    width: 5,
+    backgroundColor: 'white',
+    // backgroundColor: 'blue',
+    borderBottomLeftRadius: 20,
+  },
+  faChevronLeft: {
+    position: 'absolute',
+    left: 0,
+  },
+  lastText: {
+    marginBottom: 20,
+  },
+  textError: {
+    marginLeft: 5,
+    alignSelf: 'center',
+  },
+  textTitle: {
+    paddingHorizontal: 8,
+  },
+  group: {
+    marginLeft: 30,
+  },
+  end: {
+    alignSelf: 'flex-end',
+  },
+  senderTextContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+  recipientTextSection: {
+    flexDirection: 'row',
+    maxWidth: '75%',
+  },
+  senderTextSection: {
+    flexDirection: 'row-reverse',
+    alignSelf: 'flex-end',
+    maxWidth: '75%',
+  },
 });
 export default SmsChatWindow;
