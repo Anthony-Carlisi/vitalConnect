3a7b2517587600246cc5c3dc24edcd41a16d2f6f
Anthony Carlisi <anthonycarlisi95@gmail.com>
2022-05-10 18:45:29 -0400

    commit

--- src/components/SmsChatWindow.js
+++ src/components/SmsChatWindow.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import moment from 'moment';
 import {
   View,
@@ -23,240 +23,36 @@ import {
 
 function SmsChatWindow() {
   const [text, onChangeText] = useState('Useless Text');
-
   const [navDropDown, setNavDropDown] = useState(false);
+  const [textFooter, setTextFooter] = useState([]);
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [smsData, setSmsData] = useState([]);
+
+  useEffect(() => {
+    fetchSmsData();
+  }, []);
+
+  // Fetch smsData
+  const fetchSmsData = async () => {
+    const response = await fetch(
+      'http://192.168.1.215:5000/smsData?_sort=SentDate&_order=desc',
+    );
+    const data = await response.json();
+    setSmsData(data);
+    setIsLoading(false);
+  };
 
   const onPress = () => {
     navDropDown === false ? setNavDropDown(true) : setNavDropDown(false);
   };
 
-  const testObject = [
-    {
-      From: '+18087935161',
-      To: '+15163034649',
-      Body: 'Please take a picture of what will need repair',
-      Status: 'delivered',
-      SentDate: '2021-09-21T13:36:52Z',
-      ApiVersion: '2010-04-01',
-      NumSegments: 1,
-      ErrorCode: 0,
-      AccountSid: 'ACe6cc1e9fb822e03ff4593f00b4301639',
-      Sid: 'SMae85e9db3c4dff5b496644241365e34f',
-      Direction: 'outbound-reply',
-      Price: -0.0075,
-      PriceUnit: 'USD',
-    },
-    {
-      From: '+18087935161',
-      To: '+15163034649',
-      Body: 'Okay lets get you an estimate',
-      Status: 'delivered',
-      SentDate: '2021-09-21T13:36:52Z',
-      ApiVersion: '2010-04-01',
-      NumSegments: 1,
-      ErrorCode: 0,
-      AccountSid: 'ACe6cc1e9fb822e03ff4593f00b4301639',
-      Sid: 'SM4ef1a842e4558008664cb32919883d26',
-      Direction: 'outbound-reply',
-      Price: -0.0075,
-      PriceUnit: 'USD',
-    },
-    {
-      From: '+18087935161',
-      To: '+15163034649',
-      Body: 'Okay lets get you an estimate',
-      Status: 'delivered',
-      SentDate: '2021-09-21T13:36:52Z',
-      ApiVersion: '2010-04-01',
-      NumSegments: 1,
-      ErrorCode: 0,
-      AccountSid: 'ACe6cc1e9fb822e03ff4593f00b4301639',
-      Sid: 'SM4ef1a842e4558008664cb32919889d46',
-      Direction: 'outbound-reply',
-      Price: -0.0075,
-      PriceUnit: 'USD',
-    },
-    {
-      From: '+15163034649',
-      To: '+18087935161',
-      Body: 'Yeah',
-      Status: 'received',
-      SentDate: '2021-09-21T13:36:52Z',
-      ApiVersion: '2010-04-01',
-      NumSegments: 1,
-      ErrorCode: 0,
-      AccountSid: 'ACe6cc1e9fb822e03ff4593f00b4301639',
-      Sid: 'SMfb98b6f06ea6db04af5bdd980299944a',
-      Direction: 'inbound',
-      Price: -0.0075,
-      PriceUnit: 'USD',
-    },
-    {
-      From: '+18087935161',
-      To: '+15163034649',
-      Body: "I didn't get that. What did you say?",
-      Status: 'delivered',
-      SentDate: '2021-09-21T13:36:48Z',
-      ApiVersion: '2010-04-01',
-      NumSegments: 1,
-      ErrorCode: 0,
-      AccountSid: 'ACe6cc1e9fb822e03ff4593f00b4301639',
-      Sid: 'SMd3294f06f55b3df35c199fefb9cee119',
-      Direction: 'outbound-reply',
-      Price: -0.0075,
-      PriceUnit: 'USD',
-    },
-    {
-      From: '+18087935161',
-      To: '+15163034649',
-      Body: 'Please take a picture of what will need repair',
-      Status: 'delivered',
-      SentDate: '2021-09-21T13:36:52Z',
-      ApiVersion: '2010-04-01',
-      NumSegments: 1,
-      ErrorCode: 0,
-      AccountSid: 'ACe6cc1e9fb822e03ff4593f00b4301639',
-      Sid: 'SMae85e9db3c4dff5b456644241365e74f',
-      Direction: 'outbound-reply',
-      Price: -0.0075,
-      PriceUnit: 'USD',
-    },
-    {
-      From: '+18087935161',
-      To: '+15163034649',
-      Body: 'Okay lets get you an estimate',
-      Status: 'delivered',
-      SentDate: '2021-09-21T13:36:52Z',
-      ApiVersion: '2010-04-01',
-      NumSegments: 1,
-      ErrorCode: 0,
-      AccountSid: 'ACe6cc1e9fb822e03ff4593f00b4301639',
-      Sid: 'SM4ef1a842e4558008664cb32919183d46',
-      Direction: 'outbound-reply',
-      Price: -0.0075,
-      PriceUnit: 'USD',
-    },
-    {
-      From: '+18087935161',
-      To: '+15163034649',
-      Body: 'Okay lets get you an estimate',
-      Status: 'delivered',
-      SentDate: '2021-09-21T13:36:52Z',
-      ApiVersion: '2010-04-01',
-      NumSegments: 1,
-      ErrorCode: 0,
-      AccountSid: 'ACe6cc1e9fb822e03ff4593f00b4301639',
-      Sid: 'SM4ef1a842e2558008664cb32919883d46',
-      Direction: 'outbound-reply',
-      Price: -0.0075,
-      PriceUnit: 'USD',
-    },
-    {
-      From: '+15163034649',
-      To: '+18087935161',
-      Body: 'Yeah',
-      Status: 'received',
-      SentDate: '2021-09-21T13:36:52Z',
-      ApiVersion: '2010-04-01',
-      NumSegments: 1,
-      ErrorCode: 0,
-      AccountSid: 'ACe6cc1e9fb822e03ff4593f00b4301639',
-      Sid: 'SMfb98b6f06ea6db04af5bdd980299943c',
-      Direction: 'inbound',
-      Price: -0.0075,
-      PriceUnit: 'USD',
-    },
-    {
-      From: '+18087935161',
-      To: '+15163034649',
-      Body: "I didn't get that. What did you say?",
-      Status: 'delivered',
-      SentDate: '2021-09-21T13:36:48Z',
-      ApiVersion: '2010-04-01',
-      NumSegments: 1,
-      ErrorCode: 0,
-      AccountSid: 'ACe6cc1e9fb822e03ff4593f00b4301639',
-      Sid: 'SMd3294f06f55b3df35c199fefb9cee110',
-      Direction: 'outbound-reply',
-      Price: -0.0075,
-      PriceUnit: 'USD',
-    },
-    {
-      From: '+18087935161',
-      To: '+15163034649',
-      Body: 'Please take a picture of what will need repair',
-      Status: 'delivered',
-      SentDate: '2021-09-21T13:36:52Z',
-      ApiVersion: '2010-04-01',
-      NumSegments: 1,
-      ErrorCode: 0,
-      AccountSid: 'ACe6cc1e9fb822e03ff4593f00b4301639',
-      Sid: 'SMae85e9db3c4dff5b496644241365e74f',
-      Direction: 'outbound-reply',
-      Price: -0.0075,
-      PriceUnit: 'USD',
-    },
-    {
-      From: '+18087935161',
-      To: '+15163034649',
-      Body: 'Okay lets get you an estimate',
-      Status: 'delivered',
-      SentDate: '2021-09-21T13:36:52Z',
-      ApiVersion: '2010-04-01',
-      NumSegments: 1,
-      ErrorCode: 0,
-      AccountSid: 'ACe6cc1e9fb822e03ff4593f00b4301639',
-      Sid: 'SM4ef1a842e4558008664fb32919883d46',
-      Direction: 'outbound-reply',
-      Price: -0.0075,
-      PriceUnit: 'USD',
-    },
-    {
-      From: '+18087935161',
-      To: '+15163034649',
-      Body: 'Okay lets get you an estimate',
-      Status: 'delivered',
-      SentDate: '2021-09-21T13:36:52Z',
-      ApiVersion: '2010-04-01',
-      NumSegments: 1,
-      ErrorCode: 0,
-      AccountSid: 'ACe6cc1e9fb822e03ff4593f00b4301639',
-      Sid: 'SM4ef1a842e4558008664cb31919883d46',
-      Direction: 'outbound-reply',
-      Price: -0.0075,
-      PriceUnit: 'USD',
-    },
-    {
-      From: '+15163034649',
-      To: '+18087935161',
-      Body: 'Yeah',
-      Status: 'received',
-      SentDate: '2021-09-21T13:36:52Z',
-      ApiVersion: '2010-04-01',
-      NumSegments: 1,
-      ErrorCode: 0,
-      AccountSid: 'ACe6cc1e9fb822e03ff4593f00b4301639',
-      Sid: 'SMfb98b6f06ea6db04af5bdd980299944c',
-      Direction: 'inbound',
-      Price: -0.0075,
-      PriceUnit: 'USD',
-    },
-    {
-      From: '+18087935161',
-      To: '+15163034649',
-      Body: "I didn't get that. What did you say?",
-      Status: 'delivered',
-      SentDate: '2021-09-21T13:36:48Z',
-      ApiVersion: '2010-04-01',
-      NumSegments: 1,
-      ErrorCode: 0,
-      AccountSid: 'ACe6cc1e9fb822e03ff4593f00b4301639',
-      Sid: 'SMd3294f06f55b3df35c199fefb9cee112',
-      Direction: 'outbound-reply',
-      Price: -0.0075,
-      PriceUnit: 'USD',
-    },
-  ];
+  const testFooter = id => {
+    textFooter.includes(id)
+      ? setTextFooter(prevState => prevState.filter(stateId => stateId !== id))
+      : setTextFooter(prevState => [...prevState, id]);
+  };
+
   const imageArray = [
     'https://picsum.photos/10',
     'https://picsum.photos/20',
@@ -293,16 +89,15 @@ function SmsChatWindow() {
               );
             })}
           </View>
-          <TouchableWithoutFeedback onPress={onPress}>
-            <View style={styles.rowCenter}>
-              <Text style={styles.titleText}>The Family</Text>
+          <TouchableWithoutFeedback style={styles.rowCenter} onPress={onPress}>
+            <Text style={styles.titleText}>
+              The Family
               <FontAwesomeIcon
                 size={8}
                 icon={navDropDown === true ? faChevronDown : faChevronRight}
               />
-            </View>
+            </Text>
           </TouchableWithoutFeedback>
-
           <View
             style={[
               styles.headerNavHiddenBar,
@@ -324,27 +119,28 @@ function SmsChatWindow() {
         </View>
       </View>
       <FlatList
-        style={styles.textWindow}
-        data={testObject}
+        style={[styles.textWindow, {position: 'absolute', width: '100%'}]}
+        data={smsData}
         scrollEnabled={true}
         renderItem={({item, index}) => {
           return (
-            <View
-              style={[
-                index + 1 < testObject.length &&
-                testObject[index + 1].From !== item.From
-                  ? styles.lastText
-                  : '',
-                {marginBottom: 2},
-              ]}>
-              {/* <Text style={[styles.textTitle, styles.group]}>Vincenzo</Text> */}
+            <TouchableWithoutFeedback onPress={() => testFooter(item.Sid)}>
               <View
-                style={
-                  item.Direction === 'inbound'
-                    ? styles.recipientTextSection
-                    : styles.senderTextSection
-                }>
-                {/* {item.Direction === 'inbound' ? (
+                style={[
+                  index + 1 < smsData.length &&
+                  smsData[index + 1].From !== item.From
+                    ? styles.lastText
+                    : styles.marginText,
+                  styles.marginText,
+                ]}>
+                {/* <Text style={[styles.textTitle, styles.group]}>Vincenzo</Text> */}
+                <View
+                  style={
+                    item.Direction === 'inbound'
+                      ? styles.recipientTextSection
+                      : styles.senderTextSection
+                  }>
+                  {/* {item.Direction === 'inbound' ? (
                   <Image
                     style={styles.textAvatar}
                     source={{
@@ -354,50 +150,60 @@ function SmsChatWindow() {
                 ) : (
                   <View />
                 )} */}
-                <View
-                  style={
-                    item.Direction === 'inbound'
-                      ? styles.recipientTextContainer
-                      : styles.senderTextContainer
-                  }>
-                  {item.Direction === 'inbound' ? (
-                    <View style={styles.leftArrow} />
-                  ) : (
-                    <View style={styles.rightArrow} />
-                  )}
-                  {item.Direction === 'inbound' ? (
-                    <View style={styles.leftArrowOverlap} />
-                  ) : (
-                    <View style={styles.rightArrowOverlap} />
-                  )}
+                  <View
+                    style={
+                      item.Direction === 'inbound'
+                        ? styles.recipientTextContainer
+                        : styles.senderTextContainer
+                    }>
+                    {item.Direction === 'inbound' ? (
+                      <View style={styles.leftArrow} />
+                    ) : (
+                      <View style={styles.rightArrow} />
+                    )}
+                    {item.Direction === 'inbound' ? (
+                      <View style={styles.leftArrowOverlap} />
+                    ) : (
+                      <View style={styles.rightArrowOverlap} />
+                    )}
+                    <Text
+                      style={[
+                        item.Direction === 'inbound'
+                          ? styles.recipientText
+                          : styles.senderText,
+                        styles.text,
+                      ]}>
+                      {item.Body}
+                    </Text>
+                  </View>
+                </View>
+
+                {(index + 1 < smsData.length &&
+                  smsData[index + 1].From !== item.From) ||
+                smsData.length - 1 === index ? (
                   <Text
                     style={[
-                      item.Direction === 'inbound'
-                        ? styles.recipientText
-                        : styles.senderText,
-                      styles.text,
+                      styles.textTitle,
+                      item.Direction === 'inbound' ? '' : styles.end,
                     ]}>
-                    {item.Body}
+                    {`${
+                      item.Status.charAt(0).toUpperCase() + item.Status.slice(1)
+                    } ${moment(item.SentDate).format('h:mm A')}`}
                   </Text>
-                </View>
+                ) : (
+                  <Text
+                    style={[
+                      styles.textTitle,
+                      item.Direction === 'inbound' ? '' : styles.end,
+                      textFooter.includes(item.Sid) ? '' : styles.displayNone,
+                    ]}>
+                    {`${
+                      item.Status.charAt(0).toUpperCase() + item.Status.slice(1)
+                    } ${moment(item.SentDate).format('h:mm A')}`}
+                  </Text>
+                )}
               </View>
-
-              {(index + 1 < testObject.length &&
-                testObject[index + 1].From !== item.From) ||
-              testObject.length - 1 === index ? (
-                <Text
-                  style={[
-                    styles.textTitle,
-                    item.Direction === 'inbound' ? '' : styles.end,
-                  ]}>
-                  {`${
-                    item.Status.charAt(0).toUpperCase() + item.Status.slice(1)
-                  } ${moment(item.SentDate).format('h:mm A')}`}
-                </Text>
-              ) : (
-                <View />
-              )}
-            </View>
+            </TouchableWithoutFeedback>
           );
         }}
         keyExtractor={item => item.Sid}
@@ -417,13 +223,12 @@ const styles = StyleSheet.create({
     height: '100%',
   },
   header: {
-    // height: 50,
     flexDirection: 'row',
     justifyContent: 'center',
-    backgroundColor: '#f9f9f9',
-    overflow: 'hidden',
+    backgroundColor: 'rgba(249,249,249, 0.9)',
     borderBottomWidth: 0.001,
     borderBottomColor: '#7a7a78',
+    // position: 'absolute',
   },
   headerLeftNav: {
     alignItems: 'center',
@@ -519,7 +324,6 @@ const styles = StyleSheet.create({
     height: 15,
     width: 13,
     backgroundColor: '#e8e8ea',
-    // backgroundColor: 'green',
     borderBottomRightRadius: 10,
   },
   leftArrowOverlap: {
@@ -529,7 +333,6 @@ const styles = StyleSheet.create({
     height: 15,
     width: 4,
     backgroundColor: 'white',
-    // backgroundColor: 'blue',
     borderBottomRightRadius: 20,
   },
   rightArrow: {
@@ -539,18 +342,15 @@ const styles = StyleSheet.create({
     height: 15,
     width: 13,
     backgroundColor: '#248bf5',
-    // backgroundColor: 'green',
     borderBottomLeftRadius: 10,
   },
   rightArrowOverlap: {
-    zIndex: 0,
     alignSelf: 'flex-end',
     position: 'absolute',
     right: -5,
     height: 15,
     width: 5,
     backgroundColor: 'white',
-    // backgroundColor: 'blue',
     borderBottomLeftRadius: 20,
   },
 
@@ -568,7 +368,6 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderColor: 'red',
     marginLeft: 5,
-    // position: 'absolute',
   },
   textTitle: {
     paddingHorizontal: 12,
@@ -597,6 +396,9 @@ const styles = StyleSheet.create({
   },
   titleText: {
     fontSize: 12,
+  },
+  marginText: {
+    marginBottom: 2,
   },
 });
 export default SmsChatWindow;
