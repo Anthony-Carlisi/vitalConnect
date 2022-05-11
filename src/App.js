f9a91a9e4ebc149336d959377dac5b9ed41864a4
Anthony-Carlisi <anthonycarlisi95@gmail.com>
2022-05-11 12:00:55 -0400

    update

--- src/App.js
+++ src/App.js
@@ -2,10 +2,44 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import SmsChatWindow from './components/SmsChatWindow';
 
+import {View, Text, Button} from 'react-native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+function HomeScreen({navigation}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+function DetailsScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
 function App() {
   return (
     <NavigationContainer>
-      <SmsChatWindow />
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'Overview'}}
+        />
+        <Stack.Screen name="Details" component={SmsChatWindow} />
+      </Stack.Navigator>
+
+      {/* <SmsChatWindow /> */}
     </NavigationContainer>
   );
 }
